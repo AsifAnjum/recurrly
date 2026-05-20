b10293fa6a7358ce68ee66891315044d2185e434
Asif Anjum <asif.anjum.rabi@gmail.com>
2026-05-20 10:16:59 +0600

    feat: implement tab-based navigation layout with custom theme, icons, and data constants

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from 'react-native';
 
 export default function Home() {
   return (
-    <View className="flex-1 items-center justify-center bg-white dark:bg-slate-900">
+    <View className="flex-1 items-center justify-center bg-background">
       <Text className="text-lg font-semibold text-slate-800 dark:text-white">Home Screen</Text>
       <Link href='/(auth)/sign-in' className='text-blue-500'>Sign In</Link>
       <Link href='/(auth)/sign-up' className='text-blue-500'>Sign Up</Link>
