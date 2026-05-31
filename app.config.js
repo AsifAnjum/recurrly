926ef4aaba282999343c3320988b40a261c162f0
Asif Anjum <asif.anjum.rabi@gmail.com>
2026-06-01 03:10:05 +0600

    chore: configure EAS project ID and fix app.json plugin formatting

--- app.config.js
+++ app.config.js
@@ -5,5 +5,8 @@ export default {
   extra: {
     posthogProjectToken: process.env.POSTHOG_PROJECT_TOKEN,
     posthogHost: process.env.POSTHOG_HOST,
+    eas: {
+      projectId: "182181aa-c224-4323-98b2-e3b836d56c28",
+    },
   },
 }
